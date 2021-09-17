62e05ce56f2a198bb4aa304b28deca55c74482e8
anixudh <anirudh.varma196@gmail.com>
2021-09-17 22:45:54 +0530

    added keyboard support

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # calculator
+-> basic calculator with keyboard support - capable of handling simple arithmentic operations such as + , - , / , * 
+-> use numpad for operations if using keyboard as shift keys are not operational
+-> made using vanilla JS
--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const operation=(a,b,op)=>{
             break;
     }
     return res;
-}
+};
 
 const numberClicked=e=>{
     if(op==""){
@@ -56,7 +56,7 @@ const numberClicked=e=>{
     }
     console.log("a="+a+" op="+op+" b="+b);
     
-}
+};
 
 const operationClicked=e=>{
     if(a!="" && b!=""){
@@ -74,7 +74,7 @@ const operationClicked=e=>{
         case "div": symb="/";break;
     }
     text.textContent=a+symb;
-}
+};
 
 const resetClicked=e=>{
     text.textContent=null;
@@ -83,7 +83,7 @@ const resetClicked=e=>{
     op="";
     symb="";
     
-}
+};
 
 const equalsClicked=e=>{
     text.textContent=operation(a,b,op);
@@ -91,7 +91,7 @@ const equalsClicked=e=>{
     b="";
     op="";
     symb="";
-}
+};
 
 const backspClicked=e=>{
   if(b!="") b=b.substr(0,b.length-1);
@@ -102,19 +102,101 @@ const backspClicked=e=>{
   else if(a!="") a=a.substr(0,a.length-1);
 
   text.textContent=a+symb+b;
+};
+
+const keyPressed=e=>{
+  switch(e.keyCode){
+    case 8 : backspClicked(this); keyflag=true; break;
+    case 46: resetClicked(this); keyflag=true; break;
+    case 187:
+    case 13: equalsClicked(this); keyflag=true; break;
+    case 107: op="add"; symb="+"; text.textContent=a+symb; keyflag=true; break;
+    case 109: op="sub"; symb="-"; text.textContent=a+symb; keyflag=true; break;
+    case 106: op="mul"; symb="*"; text.textContent=a+symb; keyflag=true; break;
+    case 111: op="div"; symb="-"; text.textContent=a+symb; keyflag=true; break;
+    default: keyflag=false;
+  }
+  if(op=="" && !keyflag){
+    switch(e.keyCode){
+        case 49:
+        case 97: 
+          a=a+"1"; break;
+        case 50:
+        case 98: 
+          a=a+"2"; break;
+        case 51: 
+        case 99:
+          a=a+"3"; break;
+        case 52: 
+        case 100:
+          a=a+"4"; break;
+        case 53: 
+        case 101:
+          a=a+"5"; break;
+        case 56: 
+        case 102:
+          a=a+"6"; break;
+        case 57: 
+        case 103:
+          a=a+"7"; break;
+        case 58: 
+        case 104:
+          a=a+"8"; break;
+        case 59: 
+        case 105:
+          a=a+"9"; break;
+        case 48: 
+        case 96: a=a+"0"; break;
+    }
+    text.textContent=a;
+}
+else if(op!=null && !keyflag){
+    switch(e.keyCode){
+      case 49:
+        case 97: 
+          b=b+"1"; break;
+        case 50:
+        case 98: 
+          b=b+"2"; break;
+        case 51: 
+        case 99:
+          b=b+"3"; break;
+        case 52: 
+        case 100:
+          b=b+"4"; break;
+        case 53: 
+        case 101:
+          b=b+"5"; break;
+        case 56: 
+        case 102:
+          b=b+"6"; break;
+        case 57: 
+        case 103:
+          b=b+"7"; break;
+        case 58: 
+        case 104:
+          b=b+"8"; break;
+        case 59: 
+        case 105:
+          b=b+"9"; break;
+        case 48: 
+        case 96: b=b+"0"; break;
+    }
+    text.textContent=a+symb+b;
+  }
 }
 
-let a="", b="", op="",symb="";
+let a="", b="", op="",symb="", keyflag=false;
 const text=document.querySelector(".text-field");
 const numbers=document.querySelectorAll(".numbers");
 numbers.forEach(number=>{
     number.addEventListener('click',numberClicked);
-})
+});
 
 const operations=document.querySelectorAll(".mathOp");
 operations.forEach(operation=>{
     operation.addEventListener('click',operationClicked);
-})
+});
 
 const reset=document.querySelector(".reset");
 reset.addEventListener('click',resetClicked);
@@ -124,3 +206,5 @@ equals.addEventListener('click',equalsClicked);
 
 const backsp=document.querySelector('.backsp');
 backsp.addEventListener('click',backspClicked);
+
+window.addEventListener('keydown',keyPressed);
